e5f36514193dc7d37eca1bfb3857e27779d68434
Paula Rodríguez <41522193+paurrodri@users.noreply.github.com>
2023-09-20 15:49:20 +0200

    Remove sanitize links

--- Utils/data_download_funs.R
+++ Utils/data_download_funs.R
@@ -163,17 +163,17 @@ print_section <- function(cv, section_id, glue_template = "default"){
   
   # Take entire entries data frame and removes the links in descending order
   # so links for the same position are right next to each other in number.
-  for(i in 1:nrow(section_data)){
-    for(col in c('title', 'description_bullets')){
-      strip_res <- sanitize_links(cv, section_data[i, col])
-      section_data[i, col] <- strip_res$text
-      cv <- strip_res$cv
-    }
-  }
+  # for(i in 1:nrow(section_data)){
+    # for(col in c('title', 'description_bullets')){
+      # strip_res <- sanitize_links(cv, section_data[i, col])
+      # section_data[i, col] <- strip_res$text
+      # cv <- strip_res$cv
+    # }
+  # }
   
   print(glue::glue_data(section_data, glue_template))
   
-  invisible(strip_res$cv)
+  invisible(cv) # invisible(strip_res$cv)
 }
 
 #' @description Construct a bar chart of skills
